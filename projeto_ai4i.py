4d80ddbfa68ae7db96081cfe2cc56b2125819d7e
SDotacilio <otaciliofaria92@gmail.com>
2026-06-15 23:01:06 +0000

    15 de jun. de 2026, 20:01

--- projeto_ai4i.py
+++ projeto_ai4i.py
@@ -73,3 +73,5 @@ plt.tight_layout()
 plt.savefig('importancia_variaveis_ai4i.png', dpi=300)
 print("\n-> Gráfico 'importancia_variaveis_ai4i.png' salvo!")
 plt.show()
+
+# Teste de sincronização automática com o GitDoc
